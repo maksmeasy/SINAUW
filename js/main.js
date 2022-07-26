db420d91e1feed6dbecb7a5b839ec03c50318606
Максим Полухин Анатольевич <93430042+maksmeasy@users.noreply.github.com>
2022-07-27 01:25:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,2 @@
-$(function () {
-
-  $('.questions__accord-link').on('click', function (e) {
-    if ($(this).hasClass('questions__accord-link--active')) {
-        $(this).removeClass('questions__accord-link--active')
-        $(this).children('.questions__accord-text').slideUp()
-    }
-    else {
-        $('.questions__accord-link').removeClass('questions__accord-link--active')
-        $('.questions__accord-text').slideUp()
-        $(this).addClass('questions__accord-link--active')
-        $(this).children('.questions__accord-text').slideDown()
-    }
-  })
-
-})
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
